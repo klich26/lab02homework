5ebc735db7a8e1c6c57d901f5f23f883bfbcc1f7
klich26 <dimus0007@gmail.com>
2025-03-21 14:13:05 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,8 +5,11 @@ int main()
 {
     std::string name;
     // enter a name
+    //вводим имя
     std::cin >> name;
 
     // print name
+    //выводим приветсвие от @name
+    
     std::cout << "hello world from " << name << std::endl;
 }
